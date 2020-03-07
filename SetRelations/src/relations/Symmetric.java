8815604cc4b683aa7163d47f4adac7844e905a75
ProgrammerSusan <hatems2017@gmail.com>
2020-03-07 12:07:17 -0500

    added data file

--- SetRelations/src/relations/Symmetric.java
+++ SetRelations/src/relations/Symmetric.java
@@ -2,7 +2,10 @@ package relations;
 
 public class Symmetric {
 	public static void main(String[] args) {
-		System.out.println("hello world");
-		System.out.println("hello world");
+		
+	}
+	
+	public void isSymmetric() {
+		
 	}
 }
